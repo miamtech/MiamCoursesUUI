971b0fc4f2f1327a39e3e68d6982ab337b0c9956
Thomas Huyghues <thomas.huyghues@miam.tech>
2023-08-25 14:49:18 +0200

    Add a loader while miam is Loading, and generate a random UUID for userid in miam init

--- app/src/main/java/tech/miam/coursesUDemoApp/MainActivity.kt
+++ app/src/main/java/tech/miam/coursesUDemoApp/MainActivity.kt
@@ -1,28 +1,31 @@
 package tech.miam.coursesUDemoApp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 import com.miam.core.localisation.I18nResolver
 import com.miam.kmmMiamCore.handler.ContextHandlerInstance
 import com.miam.kmmMiamCore.handler.ReadyEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import tech.miam.coursesUDemoApp.features.miam.template.MiamSdkHelper
 import org.koin.android.ext.android.inject
 import tech.miam.coursesUDemoApp.basket.BasketLocalDataSource
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
     private val basketLocalDataSource by inject<BasketLocalDataSource>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         MiamSdkHelper.initialize(
             context = this,
             supplierId = 7,
             supplierOrigin = "app.courseu.com",
             storeId = "25910",
-            userId = "u_test_user",
+            userId = UUID.randomUUID().toString(),
             basket = basketLocalDataSource.getProducts()
         )
         I18nResolver.registerContext(this.applicationContext)
@@ -30,7 +33,8 @@ class MainActivity : AppCompatActivity() {
             ContextHandlerInstance.instance.observeReadyEvent().collect {
                 when (it) {
                     ReadyEvent.isReady -> {
-                        setContentView(R.layout.activity_main)
+                        val navHostFragment = findViewById<FragmentContainerView>(R.id.nav_host_fragment)
+                        navHostFragment.visibility = View.VISIBLE
                     }
                     else -> {}
                 }

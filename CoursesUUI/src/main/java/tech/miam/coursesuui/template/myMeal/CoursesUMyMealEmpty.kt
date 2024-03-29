de7719f675ff0e4d3a0e231b79ec41e2910141c1
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2024-03-29 16:38:31 +0100

    add my meals empty

--- CoursesUUI/src/main/java/tech/miam/coursesuui/template/myMeal/CoursesUMyMealEmpty.kt
+++ CoursesUUI/src/main/java/tech/miam/coursesuui/template/myMeal/CoursesUMyMealEmpty.kt
@@ -1,24 +1,30 @@
 package tech.miam.coursesuui.template.myMeal
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.miam.core.localisation.Localisation
+import com.miam.kmm_miam_sdk.android.theme.Colors
 import com.miam.kmm_miam_sdk.android.theme.Typography
+import com.miam.kmm_miam_sdk.android.ui.components.common.Clickable
 import com.miam.sdk.components.baseComponent.emptyPage.EmptyPage
 import com.miam.sdk.components.baseComponent.emptyPage.EmptyPageParameters
 import tech.miam.coursesuui.R
@@ -56,13 +62,18 @@ class CoursesUMyMealEmpty: EmptyPage {
                     textAlign = TextAlign.Center
                 )
                 if (params.haveAnAction) {
-                    Button(
-                        onClick = params.action
-                    ) {
-                        Text(
-                            params.actionText,
-                            style = Typography.button
-                        )
+                    Clickable(onClick = params.action) {
+                        Box(
+                            Modifier
+                                .clip(RoundedCornerShape(50))
+                                .background(Colors.primary)
+                        ) {
+                            Text(
+                                text = params.actionText,
+                                color = Colors.white,
+                                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                            )
+                        }
                     }
                 }
             }

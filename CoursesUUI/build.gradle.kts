068f9e0d7b5054fef6245ece59a446f231833597
Maxime Pezeril <maxime.pezeril@mealz.ai>
2026-01-07 18:32:49 +0100

    Upgrade to SDK 5.10.8

--- CoursesUUI/build.gradle.kts
+++ CoursesUUI/build.gradle.kts
@@ -37,8 +37,8 @@ android {
 }
 
 dependencies {
-    api("ai.mealz.core:mealz-core:5.10.7")
-    api("ai.mealz.android:mealz-android:5.10.7")
+    api("ai.mealz.core:mealz-core:5.10.8")
+    api("ai.mealz.android:mealz-android:5.10.8")
 
     api("androidx.core:core-ktx:1.17.0")
     api("androidx.appcompat:appcompat:1.7.1")
